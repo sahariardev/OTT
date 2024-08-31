15d1e8ad45008ae84a92faf09bdd48a23df01e74
sahariardev <rifatsahariar@gmail.com>
2024-08-31 19:36:15 +0600

    Fixed misc issues

--- client/youtube-client/src/app/page.js
+++ client/youtube-client/src/app/page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Room from "@/app/pages/room";
 import UploadForm from "@/app/components/uploadForm";
+import AuthPage from "@/app/pages/AuthPage";
 
 const Home = () => {
   return (
       <div>
-          <UploadForm/>
+          <AuthPage/>
       </div>
   );
 }
